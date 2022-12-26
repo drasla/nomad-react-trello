a124ac2b3bc663c81fc6ed30e0da09cbdbcceb1b
Tae.Garam <drasla@naver.com>
2022-12-26 19:06:36 +0900

    #7.3 Drag and Drop part Two

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,29 @@
 import React from "react";
-import {DragDropContext, Draggable, Droppable} from "react-beautiful-dnd";
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 
 function App() {
     const onDragEnd = () => {
         //
     };
 
-    return <DragDropContext onDragEnd={onDragEnd}>
-        <div>
-            <Droppable droppableId={"one"}>
-                {() => <ul>
-                    <Draggable draggableId={"first"} index={0}>
-                        {() => <li>One</li>}
-                    </Draggable>
-                    <Draggable draggableId={"second"} index={1}>
-                        {() => <li>Second</li>}
-                    </Draggable>
-                </ul>}
-            </Droppable>
-        </div>
-    </DragDropContext>
+    return (
+        <DragDropContext onDragEnd={onDragEnd}>
+            <div>
+                <Droppable droppableId={"one"}>
+                    {magic => (
+                        <ul ref={magic.innerRef} {...magic.droppableProps}>
+                            <Draggable draggableId={"first"} index={0}>
+                                {() => <li>One</li>}
+                            </Draggable>
+                            <Draggable draggableId={"second"} index={1}>
+                                {() => <li>Second</li>}
+                            </Draggable>
+                        </ul>
+                    )}
+                </Droppable>
+            </div>
+        </DragDropContext>
+    );
 }
 
 export default App;
